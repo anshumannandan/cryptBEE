b126aea9f01d4eb19348fc8839d622d37b7060b9
anshumannandan <anshuman21xxx034@akgec.ac.in>
2022-12-23 02:40:18 +0530

    created auth models

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id', 'name']
+    list_display = ('id', 'name', 'email')
+    list_filter = ('is_superuser',)
+    filter_horizontal = ()
+    fieldsets = (
+        ('User Credentials', {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_superuser', 'is_staff',)}),
+    )
+    add_fieldsets = (
+        ('User Credentials', {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_superuser', 'is_staff',)}),
+    )
+
+
+admin.site.register(User, UserAdmin)
--- Authentication/models.py
+++ Authentication/models.py
@@ -1,3 +1,67 @@
-from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import BooleanField, EmailField, BigIntegerField, CharField
+from django.db.models.fields.related import OneToOneField
+from django.db.models import CASCADE
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+    email = EmailField(max_length=255, unique=True)
+    name = CharField(max_length=255)
+
+    is_superuser = BooleanField(default=False)
+    is_staff = BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'email']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+
+    def refresh(self):
+        refresh = RefreshToken.for_user(self)
+        return str(refresh)
+
+    def access(self):
+        refresh = RefreshToken.for_user(self)
+        return str(refresh.access_token)
+
+
+class Two_Factor_Verification(Model):
+    user = OneToOneField(User, on_delete=CASCADE, related_name='twofactor')
+    phone_number = BigIntegerField(unique=True,
+                                   validators=[MinValueValidator(1000000000), MaxValueValidator(9999999999)])
+
+
+class PAN_Verification(Model):
+    user = OneToOneField(User, on_delete=CASCADE, related_name='pan_details')
+    pan_number = CharField(unique=True, max_length=10,
+                           validators=[RegexValidator(regex='[A-Z]{5}[0-9]{4}[A-Z]{1}',
+                                                      message='Invalid PAN',),])
