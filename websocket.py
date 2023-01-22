28e115ff6a3188f0c2c33fda1082f28342e9273e
anshumannandan <anshuman21xxx034@akgec.ac.in>
2023-01-22 18:23:13 +0530

    my holdings in websocket

--- websocket.py
+++ websocket.py
@@ -7,12 +7,13 @@ from asgiref.sync import sync_to_async
 from django.conf import settings
 import jwt
 
-import logging
-logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+# import logging
+# logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryptBEE.settings')
 django.setup()
 
+from Authentication.models import User
 from Investments.models import Coin
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
@@ -32,29 +33,41 @@ def RemoveFromCeleryBeat():
         task.delete()
 
 
-async def socket(websocket):
+@sync_to_async
+def holdings_data(user):
+    holdings = []
+    try:
+        for holding in user.my_holdings.MyHoldings:
+            coin = Coin.objects.get(Name = holding[0])
+            holdings.append({"Name" : coin.Name, "FullName": coin.FullName, "ImageURL" : coin.Image, "Coins" : holding[1], "Price" : holding[2]})
+    except: pass
+    return holdings
+
+
+async def coins_data(websocket, user):
     try:
         coins = Coin.objects.all()
         while True:
             data = []
             async for coin in coins:
                 data.append({'Name': coin.Name, 'FullName' : coin.FullName, 'Price': coin.Price, 'ChangePct': coin.ChangePct, 'ImageURL': coin.Image})
-            await websocket.send(json.dumps({'data': data}))
+            holdings = await holdings_data(user)
+            await websocket.send(json.dumps({'data': data, 'holdings' : holdings}))
             await asyncio.sleep(10)
     except websockets.ConnectionClosedOK:
         return
 
 
-async def handler(websocket):
+async def handler(websocket, user):
     global connected
     connected.add(websocket)
     if len(connected) == 1:
         await AddToCeleryBeat()
-    await socket(websocket)
+    await coins_data(websocket, user)
     connected.remove(websocket)
     if len(connected) == 0:
         await RemoveFromCeleryBeat()
-    await websocket.send('authorised, enter ALL or name of the coin')
+    # await websocket.send('authorised, enter ALL or name of the coin')
     # req = await websocket.recv()
     # if req == 'ALL':
     #     await socket(websocket)
@@ -62,14 +75,21 @@ async def handler(websocket):
     #     return
 
 
+@sync_to_async
+def get_user(tokenset):
+    return User.objects.get(id = tokenset['user_id'])
+
+
 async def authorise(websocket):
     await websocket.send('connection established, send token to recieve data')    
     token = await websocket.recv()
     try: 
-        jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-        await handler(websocket)
+        tokenset = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
     except:
         await websocket.send('invalid token')
+        return
+    user = await get_user(tokenset)
+    await handler(websocket, user)
 
 
 async def main():
